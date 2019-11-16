1055b13ec0f7f69a5b00c01bc952dd593d07c241
Gi@da <giadavolpi8@gmail.com>
2019-11-16 19:13:41 +0100

    aggiustare stile

--- main.js
+++ main.js
@@ -2,7 +2,36 @@
 //Attraverso un prompt chiedere all'utente un numero tra 1 e 6.
 //Stampare un messaggio per dire all'utente se ha vinto oppure no (il numero più alto vince).
 
-
-alert ("Sfida a dadi il tuo pc! Lancia il tuo dado cliccando su ok")
+// chiedo all'utente il suo nome e se vuole giocare a dadi
+var nomeUtente = prompt ("Ehi ciao! Come ti chiami? SCRIVIMI IL TUO NOME");
+console.log (nomeUtente);
+alert ("Piacere" + " " + nomeUtente + ". " + "Io mi chiamo Giada. Hai voglia di giocare a dadi con me?")
+alert ("clicca su OK per lanciare il tuo dado");
+// variabile per estrazione numero utente
 var dadoUtente = Math.floor(Math.random() * 6) + 1;
 console.log (dadoUtente);
+// comunico all'utente quale numero è uscito dal suo lancio
+alert ("Il numero estratto è il" + " " + dadoUtente);
+alert ("Se mi dai l'OK lancio il mio dado")
+// variabile per estrazione numero PC
+var dadoPC = Math.floor(Math.random() * 6) + 1;
+console.log (dadoPC);
+
+document.getElementById ("num-utente").innerHTML = dadoUtente;
+document.getElementById ("num-pc").innerHTML = dadoPC;
+
+if (dadoUtente < dadoPC) {
+    document.getElementById ("nome-utente").innerHTML = nomeUtente;
+    document.getElementById ("vinto-perso").innerHTML = "Hai perso";
+    document.getElementById ("container-dado-utente").setAttribute ("class", "looser");
+    document.getElementById ("container-dado-pc").setAttribute ("class", "winner");
+} else if (dadoUtente > dadoPC) {
+    document.getElementById ("nome-utente").innerHTML = nomeUtente;
+    document.getElementById ("vinto-perso").innerHTML = "Hai vinto!!";
+    document.getElementById ("container-dado-utente").setAttribute ("class", "winner");
+    document.getElementById ("container-dado-pc").setAttribute ("class", "looser");
+} else if (dadoUtente === dadoPC) {
+    document.getElementById ("nome-utente").innerHTML = nomeUtente;
+    document.getElementById ("container-dado-utente").setAttribute ("class", "equality");
+    document.getElementById ("container-dado-pc").setAttribute ("class", "equality");
+}
