55539ed897ce084b491f8e16353712206e3732d1
Gi@da <giadavolpi8@gmail.com>
2019-11-17 14:13:09 +0100

    end

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-//Generare un numero casuale tra 1 e 6.
-//Attraverso un prompt chiedere all'utente un numero tra 1 e 6.
-//Stampare un messaggio per dire all'utente se ha vinto oppure no (il numero più alto vince).
-
 // chiedo all'utente il suo nome e se vuole giocare a dadi
 var nomeUtente = prompt ("Ehi ciao! Come ti chiami? SCRIVIMI IL TUO NOME");
 console.log (nomeUtente);
